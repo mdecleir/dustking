49b3523d93e2f1d1b47e1b7c1b2ee86641a86f5a
mdecleir <mdecleir@stsci.edu>
2022-08-24 17:26:10 -0400

    Changed TIR calculation to luminosities

--- create_cigale_cat.py
+++ create_cigale_cat.py
@@ -7,15 +7,192 @@
 # Import the necessary packages
 import numpy as np
 import pandas as pd
-import astropy.units as u
+
 from astropy.constants import c
 from astropy.table import Table
 
 
+def ext_corr(flux_table, filters, ext_factor, galaxy):
+    """
+    Function to correct the fluxes and uncertainties for the MW foreground extinction
+    """
+    EBV = flux_table.loc[galaxy, "E(B-V)"]
+    for i, filter in enumerate(filters):
+        # Correct the flux and uncertainty for the MW extinction
+        A_lambda = ext_factor[i] * EBV * 3.1
+        flux_table.loc[galaxy, filter] *= 10 ** (0.4 * A_lambda)
+        flux_table.loc[galaxy, filter + "_err"] *= 10 ** (0.4 * A_lambda)
+
+        # Convert the units from Jy to mJy
+        flux_table.loc[galaxy, filter] *= 10 ** 3.0
+        flux_table.loc[galaxy, filter + "_err"] *= 10 ** 3.0
+
+
+def optical(flux_table, galaxy):
+    """
+    Function to exclude the BVRI fluxes if SDSS fluxes are available
+    """
+    BVRI = ["B", "V", "R", "I", "B_err", "V_err", "R_err", "I_err"]
+    if not np.isnan(flux_table.loc[galaxy, "u"]):
+        for f in BVRI:
+            flux_table.loc[galaxy, f] = "NaN"
+
+
+def TIR(flux_table, filters, galaxy):
+    """
+    Function to calculate the TIR luminosity based on the Galametz+2013 calibrations (table 3, set 2)
+    """
+    # Define the filters to be used in the calculation, the wavelengths of these filters (from Dale+2017), and the coefficients used in the calculation
+    filters = ["MIPS24", "PACS70", "PACS100", "PACS160", "SPIRE250"]
+    wavelengths = [23.8e-6, 71.8e-6, 103.0e-6, 167.0e-6, 252.0e-6]
+    coeffs = np.array([2.023, 0.523, 0.390, 0.577, 0.721])
+    coeffs_err = np.array([0.082, 0.028, 0.021, 0.036, 0.070])
+    L = np.zeros(5)
+    L_err = np.zeros(5)
+
+    # Convert the IR flux densities and their uncertainties (in Jy) to solar luminosities (L_sol)
+    # 1 Jy = 1e-26 * 4 * pi * (D[m])^2 * c / lambda / 3.828e26 L_sol
+    for i, filter in enumerate(filters):
+        L[i] = (
+            flux_table.loc[galaxy, filter]
+            * 1e-26
+            * 4
+            * np.pi
+            * (flux_table.loc[galaxy, "D(Mpc)"] * 1e6 * 3.08567758128e16) ** 2
+            * c.value
+            / wavelengths[i]
+            / 3.828e26
+        )
+        L_err[i] = (
+            flux_table.loc[galaxy, filter + "_err"]
+            * 1e-26
+            * 4
+            * np.pi
+            * (flux_table.loc[galaxy, "D(Mpc)"] * 1e6 * 3.08567758128e16) ** 2
+            * c.value
+            / wavelengths[i]
+            / 3.828e26
+        )
+
+    # Calculate the TIR luminosity (in L_sol)
+    LTIR = np.sum(coeffs * L)
+    LTIR_err = np.sqrt(np.sum(L ** 2 * coeffs_err ** 2 + coeffs ** 2 * L_err ** 2))
+
+    # Convert the units of the TIR luminosity from L_sol to W
+    flux_table.loc[galaxy, "TIR(W)"] = LTIR * 3.828e26
+    flux_table.loc[galaxy, "TIR_err(W)"] = LTIR_err * 3.828e26
+
+
+def write_line(frame, flux_table, galaxy):
+    """
+    Function to write all fluxes, uncertainties and other input parameters of a certain galaxy into the data frame
+    """
+    frame.loc[galaxy] = [
+        galaxy,
+        0.0,
+        flux_table.loc[galaxy, "FUV"],
+        flux_table.loc[galaxy, "UVW2"],
+        flux_table.loc[galaxy, "UVM2"],
+        flux_table.loc[galaxy, "NUV"],
+        flux_table.loc[galaxy, "UVW1"],
+        flux_table.loc[galaxy, "u"],
+        flux_table.loc[galaxy, "B"],
+        flux_table.loc[galaxy, "g"],
+        flux_table.loc[galaxy, "V"],
+        flux_table.loc[galaxy, "r"],
+        flux_table.loc[galaxy, "R"],
+        flux_table.loc[galaxy, "i"],
+        flux_table.loc[galaxy, "I"],
+        flux_table.loc[galaxy, "z"],
+        flux_table.loc[galaxy, "J"],
+        flux_table.loc[galaxy, "H"],
+        flux_table.loc[galaxy, "K"],
+        flux_table.loc[galaxy, "I1"],
+        flux_table.loc[galaxy, "I2"],
+        flux_table.loc[galaxy, "FUV_err"],
+        flux_table.loc[galaxy, "UVW2_err"],
+        flux_table.loc[galaxy, "UVM2_err"],
+        flux_table.loc[galaxy, "NUV_err"],
+        flux_table.loc[galaxy, "UVW1_err"],
+        flux_table.loc[galaxy, "u_err"],
+        flux_table.loc[galaxy, "B_err"],
+        flux_table.loc[galaxy, "g_err"],
+        flux_table.loc[galaxy, "V_err"],
+        flux_table.loc[galaxy, "r_err"],
+        flux_table.loc[galaxy, "R_err"],
+        flux_table.loc[galaxy, "i_err"],
+        flux_table.loc[galaxy, "I_err"],
+        flux_table.loc[galaxy, "z_err"],
+        flux_table.loc[galaxy, "J_err"],
+        flux_table.loc[galaxy, "H_err"],
+        flux_table.loc[galaxy, "K_err"],
+        flux_table.loc[galaxy, "I1_err"],
+        flux_table.loc[galaxy, "I2_err"],
+        flux_table.loc[galaxy, "TIR(W)"],
+        flux_table.loc[galaxy, "TIR_err(W)"],
+        flux_table.loc[galaxy, "D(Mpc)"],
+    ]
+
+
+def write_line_without_SWIFT(frame, flux_table, galaxy):
+    """
+    Function to write all fluxes, uncertainties and other input parameters of a certain galaxy into the data frame, but without Swift values
+    """
+    frame.loc[galaxy] = [
+        galaxy,
+        0.0,
+        flux_table.loc[galaxy, "FUV"],
+        "NaN",
+        "NaN",
+        flux_table.loc[galaxy, "NUV"],
+        "NaN",
+        flux_table.loc[galaxy, "u"],
+        flux_table.loc[galaxy, "B"],
+        flux_table.loc[galaxy, "g"],
+        flux_table.loc[galaxy, "V"],
+        flux_table.loc[galaxy, "r"],
+        flux_table.loc[galaxy, "R"],
+        flux_table.loc[galaxy, "i"],
+        flux_table.loc[galaxy, "I"],
+        flux_table.loc[galaxy, "z"],
+        flux_table.loc[galaxy, "J"],
+        flux_table.loc[galaxy, "H"],
+        flux_table.loc[galaxy, "K"],
+        flux_table.loc[galaxy, "I1"],
+        flux_table.loc[galaxy, "I2"],
+        flux_table.loc[galaxy, "FUV_err"],
+        "NaN",
+        "NaN",
+        flux_table.loc[galaxy, "NUV_err"],
+        "NaN",
+        flux_table.loc[galaxy, "u_err"],
+        flux_table.loc[galaxy, "B_err"],
+        flux_table.loc[galaxy, "g_err"],
+        flux_table.loc[galaxy, "V_err"],
+        flux_table.loc[galaxy, "r_err"],
+        flux_table.loc[galaxy, "R_err"],
+        flux_table.loc[galaxy, "i_err"],
+        flux_table.loc[galaxy, "I_err"],
+        flux_table.loc[galaxy, "z_err"],
+        flux_table.loc[galaxy, "J_err"],
+        flux_table.loc[galaxy, "H_err"],
+        flux_table.loc[galaxy, "K_err"],
+        flux_table.loc[galaxy, "I1_err"],
+        flux_table.loc[galaxy, "I2_err"],
+        flux_table.loc[galaxy, "TIR(W)"],
+        flux_table.loc[galaxy, "TIR_err(W)"],
+        flux_table.loc[galaxy, "D(Mpc)"],
+    ]
+
+
 def main():
+    # Define the data path and the output path
+    data_path = "/Users/mdecleir/Documents/DustKING/Data/"
+    out_path = "/Users/mdecleir/Documents/DustKING/Cigale_fitting/"
+
     # Read the Excel file with the fluxes, uncertainties, and other input parameters
     flux_table = pd.read_excel(
-        "/Users/mdecleir/Documents/DustKING/Data/KINGFISH_fluxes.xlsx",
+        data_path + "KINGFISH_fluxes.xlsx",
         sheet_name="fluxes_new",
         index_col=0,
     )
@@ -163,8 +340,6 @@ def main():
     )
 
     for galaxy in flux_table.index:
-        print(galaxy)
-
         # Apply the MW extinction correction
         ext_corr(flux_table, filters, ext_factor, galaxy)
 
@@ -177,181 +352,12 @@ def main():
         write_line(frame_with, flux_table, galaxy)
         write_line_without_SWIFT(frame_without, flux_table, galaxy)
 
-    Table.from_pandas(frame_with).write("DustKING.cat", format="ascii", overwrite=True)
+    Table.from_pandas(frame_with).write(
+        out_path + "DustKING.cat", format="ascii", overwrite=True
+    )
     Table.from_pandas(frame_without).write(
-        "DustKING_noSWIFT.cat", format="ascii", overwrite=True
+        out_path + "DustKING_noSWIFT.cat", format="ascii", overwrite=True
     )
-
-
-def ext_corr(flux_table, filters, ext_factor, galaxy):
-    """
-    Function to correct the fluxes and uncertainties for the MW foreground extinction
-    """
-    EBV = flux_table.loc[galaxy, "E(B-V)"]
-    for i, filter in enumerate(filters):
-        # Correct the flux and uncertainty for the MW extinction
-        A_lambda = ext_factor[i] * EBV * 3.1
-        flux_table.loc[galaxy, filter] *= 10 ** (0.4 * A_lambda)
-        flux_table.loc[galaxy, filter + "_err"] *= 10 ** (0.4 * A_lambda)
-
-        # Convert the units from Jy to mJy
-        flux_table.loc[galaxy, filter] *= 10 ** 3.0
-        flux_table.loc[galaxy, filter + "_err"] *= 10 ** 3.0
-
-
-def optical(flux_table, galaxy):
-    """
-    Function to exclude the BVRI fluxes if SDSS fluxes are available
-    """
-    BVRI = ["B", "V", "R", "I", "B_err", "V_err", "R_err", "I_err"]
-    if not np.isnan(flux_table.loc[galaxy, "u"]):
-        for f in BVRI:
-            flux_table.loc[galaxy, f] = "NaN"
-
-
-def TIR(flux_table, filters, galaxy):
-    """
-    Function to calculate the TIR luminosity based on the Galametz+2013 calibrations (table 3)
-    """
-    # Define the filters to be used in the calculation, the wavelengths of these filters, and the coefficients used in the calculation
-    filters = ["MIPS24", "PACS70", "PACS100", "PACS160", "SPIRE250"]
-    wavelengths = [23.8e-6, 71.8e-6, 103.0e-6, 167.0e-6, 252.0e-6]
-    coeff = np.array([2.013, 0.508, 0.393, 0.599, 0.68])
-    coeff_err = np.array([0.081, 0.029, 0.025, 0.042, 0.078])
-    S = np.zeros(5)
-    S_err = np.zeros(5)
-
-    # Convert the units of the IR fluxes and their uncertainties from Jy to W/kpc^2
-    # 1 Jy = 1e-26 * c / lambda * (3.08567758e19)^2 W/kpc^2
-    for i, filter in enumerate(filters):
-        S[i] = (
-            flux_table.loc[galaxy, filter]
-            * 1e-26
-            * c.value
-            / wavelengths[i]
-            * (3.08567758e19) ** 2
-        )
-        S_err[i] = (
-            flux_table.loc[galaxy, filter + "_err"]
-            * 1e-26
-            * c.value
-            / wavelengths[i]
-            * (3.08567758e19) ** 2
-        )
-
-    # Calculate the TIR surface brightness (in W/kpc^2)
-    S_TIR = np.sum(coeff * S)
-    S_TIR_err = np.sqrt(np.sum(S ** 2 * coeff_err ** 2 + coeff ** 2 * S_err ** 2))
-
-    # Convert the units of the TIR from W/kpc^2 to W
-    flux_table.loc[galaxy, "TIR(W)"] = (
-        S_TIR * 4 * np.pi * (flux_table.loc[galaxy, "D(Mpc)"] * 1e3) ** 2
-    )
-    flux_table.loc[galaxy, "TIR_err(W)"] = (
-        S_TIR_err * 4 * np.pi * (flux_table.loc[galaxy, "D(Mpc)"] * 1e3) ** 2
-    )
-
-
-def write_line(frame, flux_table, galaxy):
-    """
-    Function to write all fluxes, uncertainties and other input parameters of a certain galaxy into the data frame
-    """
-    frame.loc[galaxy] = [
-        galaxy,
-        0.0,
-        flux_table.loc[galaxy, "FUV"],
-        flux_table.loc[galaxy, "UVW2"],
-        flux_table.loc[galaxy, "UVM2"],
-        flux_table.loc[galaxy, "NUV"],
-        flux_table.loc[galaxy, "UVW1"],
-        flux_table.loc[galaxy, "u"],
-        flux_table.loc[galaxy, "B"],
-        flux_table.loc[galaxy, "g"],
-        flux_table.loc[galaxy, "V"],
-        flux_table.loc[galaxy, "r"],
-        flux_table.loc[galaxy, "R"],
-        flux_table.loc[galaxy, "i"],
-        flux_table.loc[galaxy, "I"],
-        flux_table.loc[galaxy, "z"],
-        flux_table.loc[galaxy, "J"],
-        flux_table.loc[galaxy, "H"],
-        flux_table.loc[galaxy, "K"],
-        flux_table.loc[galaxy, "I1"],
-        flux_table.loc[galaxy, "I2"],
-        flux_table.loc[galaxy, "FUV_err"],
-        flux_table.loc[galaxy, "UVW2_err"],
-        flux_table.loc[galaxy, "UVM2_err"],
-        flux_table.loc[galaxy, "NUV_err"],
-        flux_table.loc[galaxy, "UVW1_err"],
-        flux_table.loc[galaxy, "u_err"],
-        flux_table.loc[galaxy, "B_err"],
-        flux_table.loc[galaxy, "g_err"],
-        flux_table.loc[galaxy, "V_err"],
-        flux_table.loc[galaxy, "r_err"],
-        flux_table.loc[galaxy, "R_err"],
-        flux_table.loc[galaxy, "i_err"],
-        flux_table.loc[galaxy, "I_err"],
-        flux_table.loc[galaxy, "z_err"],
-        flux_table.loc[galaxy, "J_err"],
-        flux_table.loc[galaxy, "H_err"],
-        flux_table.loc[galaxy, "K_err"],
-        flux_table.loc[galaxy, "I1_err"],
-        flux_table.loc[galaxy, "I2_err"],
-        flux_table.loc[galaxy, "TIR(W)"],
-        flux_table.loc[galaxy, "TIR_err(W)"],
-        flux_table.loc[galaxy, "D(Mpc)"],
-    ]
-
-
-def write_line_without_SWIFT(frame, flux_table, galaxy):
-    """
-    Function to write all fluxes, uncertainties and other input parameters of a certain galaxy into the data frame, but without Swift values
-    """
-    frame.loc[galaxy] = [
-        galaxy,
-        0.0,
-        flux_table.loc[galaxy, "FUV"],
-        "NaN",
-        "NaN",
-        flux_table.loc[galaxy, "NUV"],
-        "NaN",
-        flux_table.loc[galaxy, "u"],
-        flux_table.loc[galaxy, "B"],
-        flux_table.loc[galaxy, "g"],
-        flux_table.loc[galaxy, "V"],
-        flux_table.loc[galaxy, "r"],
-        flux_table.loc[galaxy, "R"],
-        flux_table.loc[galaxy, "i"],
-        flux_table.loc[galaxy, "I"],
-        flux_table.loc[galaxy, "z"],
-        flux_table.loc[galaxy, "J"],
-        flux_table.loc[galaxy, "H"],
-        flux_table.loc[galaxy, "K"],
-        flux_table.loc[galaxy, "I1"],
-        flux_table.loc[galaxy, "I2"],
-        flux_table.loc[galaxy, "FUV_err"],
-        "NaN",
-        "NaN",
-        flux_table.loc[galaxy, "NUV_err"],
-        "NaN",
-        flux_table.loc[galaxy, "u_err"],
-        flux_table.loc[galaxy, "B_err"],
-        flux_table.loc[galaxy, "g_err"],
-        flux_table.loc[galaxy, "V_err"],
-        flux_table.loc[galaxy, "r_err"],
-        flux_table.loc[galaxy, "R_err"],
-        flux_table.loc[galaxy, "i_err"],
-        flux_table.loc[galaxy, "I_err"],
-        flux_table.loc[galaxy, "z_err"],
-        flux_table.loc[galaxy, "J_err"],
-        flux_table.loc[galaxy, "H_err"],
-        flux_table.loc[galaxy, "K_err"],
-        flux_table.loc[galaxy, "I1_err"],
-        flux_table.loc[galaxy, "I2_err"],
-        flux_table.loc[galaxy, "TIR(W)"],
-        flux_table.loc[galaxy, "TIR_err(W)"],
-        flux_table.loc[galaxy, "D(Mpc)"],
-    ]
 
 
 if __name__ == "__main__":
